a08da45b9f2b315d605eace0ff065a8f4b2a3eec
manfromsiberia <manfromsiberia@yandex.ru>
2018-04-26 20:57:39 +0300

    add some effects and animation

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {
   Text,
   StatusBar,
   View, DrawerLayoutAndroid, ScrollView, AsyncStorage, TouchableHighlight, Animated, Modal, Dimensions,
-  TouchableNativeFeedback
+  TouchableNativeFeedback, ToastAndroid
 } from 'react-native';
 import Toolbar from './components/Toolbar'
 import ToDoItem from "./components/ToDoItem";
@@ -30,7 +30,6 @@ export default class App extends Component<Props> {
 
   constructor (){
     super();
-    this.openDrawer = this.openDrawer.bind(this);
     this.addNewPoint = this.addNewPoint.bind(this);
   }
   //clear = AsyncStorage.clear();
@@ -47,37 +46,67 @@ export default class App extends Component<Props> {
     }
   };
 
-  addNewPoint () {
+  static generateId(){
+    let token = '';
+    const possible = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789';
+    const count = possible.length - 1;
+    let position;
+    for( let i = 0; i < 30; i++ ) {
+      position = Math.round(Math.random() * count);
+      token += possible.charAt(position);
+    }
+    return token;
+  }
+
+  addNewPoint (inputTitle, inputDescription) {
     //this.onOpenModal();
-    let title = "titletest";
-    let description = "descriptiontest";
-    const newPoint = {title, description};
+    let title = inputTitle;
+    let description = inputDescription;
+    let id = App.generateId();
+    let complete = false;
+
+    const newPoint = {title, description, id, complete};
     this.setName(newPoint).then(()=>{
       this.setState({todos: [newPoint, ...this.state.todos]});
+      this.setState({modalVisible: false});
+      this.setState({title:''});
+      this.setState({description:''});
+      ToastAndroid.show('Successful created new task!', ToastAndroid.SHORT);
     }).catch(err => {
       alert(err)
     });
   }
 
-  openDrawer (){
-    this.refs.leftMenu.openDrawer();
-  };
-
-  onOpenModal(){
-      this.setState({modalVisible: true})
+  testFunc(){
+      alert(JSON.stringify(this.state.todos))
   }
 
-
+  updateItem = (value, action) => {
+    //alert(this.state.todos[0].id);
+    this.state.todos.map(element =>{
+      if (element.id === value){
+        //alert(element.title);
+        let count = this.state.todos.indexOf(element);
+        if (action === 'COMPLETE'){
+          let buff = this.state.todos;
+          buff[count].complete = !buff[count].complete;
+          this.setState({todos: buff});
+          AsyncStorage.setItem('todos', JSON.stringify(this.state.todos));
+        }else if(action === 'DELETE'){
+          let buff = this.state.todos;
+          buff.splice(count,1);
+          this.setState({todos: buff});
+          AsyncStorage.setItem('todos', JSON.stringify(this.state.todos));
+        }
+      }
+    })
+  };
   render() {
     let { title } = this.state;
     let { description } = this.state;
     return (
-        <DrawerLayoutAndroid ref={'leftMenu'}
-                             renderNavigationView={() => (<View>
-                             </View>)}
-                             drawerPosition={DrawerLayoutAndroid.positions.Left}>
-          <StatusBar translucent backgroundColor={"#0d7b9c"}/>
         <View style={styles.container}>
+          <StatusBar translucent backgroundColor={"#0d7b9c"}/>
 
           <Modal  transparent={true}
                   visible={this.state.modalVisible}
@@ -108,7 +137,8 @@ export default class App extends Component<Props> {
                   <RaisedTextButton
                     title='Create'
                     titleColor='#ffffff'
-                    color='rgb(0, 145, 234)'/>
+                    color='rgb(0, 145, 234)'
+                    onPress={()=> {this.addNewPoint(title, description)}}/>
                 </View>
               </View>
 
@@ -117,17 +147,18 @@ export default class App extends Component<Props> {
           </Modal>
 
           <Toolbar shadow
-                   rightOptions={[{icon: 'add', handler:()=> {this.setState({modalVisible: true})}}]}
-                    leftOption={{icon: 'menu', handler: this.openDrawer}}/>
-          <TouchableHighlight onPress={()=>{this.setState({modalVisible: true})}}>
-            <Text>Open Modal Form</Text>
-          </TouchableHighlight>
+                   rightOptions={[{icon: 'add', handler:()=> {this.setState({modalVisible: true})}}]}/>
           <View style={styles.content}>
             {this.state.todos
               ?
               <ScrollView>
                 {this.state.todos.map(element =>
-                  <ToDoItem key={element} title={element.title} description={element.description}/>)}
+                  <ToDoItem key={element}
+                            updateItem={this.updateItem}
+                            id={element.id}
+                            title={element.title}
+                            description={element.description}
+                            complete={element.complete}/>)}
               </ScrollView>
               :
               <View>
@@ -136,18 +167,8 @@ export default class App extends Component<Props> {
             }
           </View>
         </View>
-        </DrawerLayoutAndroid>
     );
   }
-}
-
-let token = '';
-const possible = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789';
-const count = possible.length - 1;
-let position;
-for( let i = 0; i < 30; i++ ) {
-  position = Math.round(Math.random() * count);
-  token += possible.charAt(position);
 }
 
 const styles = StyleSheet.create({
--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -8,10 +8,13 @@ import Icon from "react-native-vector-icons/MaterialIcons";
 
 const {height, width} = Dimensions.get('window');
 let showDescription = false;
+const leftContent = <Text>Pull to activate</Text>;
 
 export default class ToDoItem extends React.PureComponent {
   state = {
-    showDescription: false
+    showDescription: false,
+    leftActionActivated: false,
+    toggle: false
   };
   componentWillMount(){
     this.animatedValue = new Animated.Value(height*0.1);
@@ -35,22 +38,40 @@ export default class ToDoItem extends React.PureComponent {
       showDescription = this.animatedValue._value === height * 0.3;
     })
   }
-
   _onPress(){
     this.heightAnimation();
   }
 
+  leftSwipe(){
+    alert("lefttt")
+  }
+
   render() {
     const animatedStyle = { height: this.animatedValue };
-    const { title, description } = this.props;
+    const { title, description, id, complete } = this.props;
+
     return (
-      <Swipeable rightButtons={rightButtons}>
-        <TouchableNativeFeedback onPress={() => this._onPress()}>
+      <Swipeable leftActionActivationDistance={125}
+                 leftContent={(
+                   <View style={[styles.leftSwipeItem]}>
+                     {complete ?
+                       <Icon name={'done'} size={40} color="green"/> :
+                       <Icon name={'clear'} size={40} color="red"/>}
+                   </View>
+                                )}
+                 onLeftActionActivate={() => {this.props.updateItem(id, 'COMPLETE')}}
+                 rightButtons={[
+          <TouchableOpacity style={styles.button} onPress={() => {this.props.updateItem(id, 'DELETE')}}><Icon name={'delete'} size={40} color="red"/></TouchableOpacity>
+        ]}>
+        <TouchableOpacity onPress={() => this._onPress()} activeOpacity={0.7}>
           <Animated.View style={[styles.item, animatedStyle]}>
-            <Text>{title}</Text>
-            {this.state.showDescription && <Text>{description}</Text>}
+            <Text>{complete? "✔":""}{title}</Text>
+            {
+              this.state.showDescription &&
+              <Text>{description}</Text>
+            }
           </Animated.View>
-        </TouchableNativeFeedback>
+        </TouchableOpacity>
       </Swipeable>
     )
 
@@ -64,20 +85,24 @@ const styles = StyleSheet.create({
       backgroundColor: '#f4f4f4',
       height: height*0.1,
       padding: 10,
-      marginHorizontal: 10,
-      marginVertical: 5,
+      marginVertical: 2,
       justifyContent: 'center',
-      borderRadius: 5,
     },
     button:{
       flex: 1,
       width: '10%',
       justifyContent: 'center',
       alignItems: 'center',
-    }
+    },
+  leftSwipeItem: {
+    flex: 1,
+    height: height*0.1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
   }
 );
 
 const rightButtons = [
-  <TouchableOpacity style={styles.button}><Icon name={'delete'} size={40} color="red"/></TouchableOpacity>
+  <TouchableOpacity style={styles.button} onPress={() => alert({title})}><Icon name={'delete'} size={40} color="red"/></TouchableOpacity>
 ];
